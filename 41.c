9fe79230077cf18452cc71dc411d3c2102e404bd
Tanishq Kumar <tanishq2008kumar@gmail.com>
2026-09-15 17:15:17 +0530

    Add files via upload

---
+++ 41.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() { int num, first, last, temp, divisor = 1, result;
+
+printf("Enter a number: ");
+scanf("%d", &num);
+
+temp = num;
+last = num % 10;
+
+while (temp >= 10) {
+    temp = temp / 10;
+    divisor = divisor * 10;
+}
+
+first = temp;
+
+result = last * divisor
+       + (num % divisor - last)
+       + first;
+
+printf("Number after swapping first and last digit = %d\n", result);
+
+return 0;
+}
